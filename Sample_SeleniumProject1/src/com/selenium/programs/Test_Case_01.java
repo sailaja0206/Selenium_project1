28a96b0050aa23738d56e1b0aca1416e8969e901
sailaja0206 <sailajap9515@gmail.com>
2022-10-25 11:40:32 +0530

    Create Test_Case_01.java
    First commit

---
+++ Sample_SeleniumProject1/src/com/selenium/programs/Test_Case_01.java
@@ -0,0 +1,11 @@
+package com.selenium.programs;
+
+public class Test_Case_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is java with selenium project");
+
+	}
+
+}
