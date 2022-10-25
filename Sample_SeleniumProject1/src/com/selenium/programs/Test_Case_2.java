829030b6977e788bd622615c8d0f5d50a1b393ec
sailaja0206 <sailajap9515@gmail.com>
2022-10-25 11:46:19 +0530

    Create Test_Case_2.java
    second test case

---
+++ Sample_SeleniumProject1/src/com/selenium/programs/Test_Case_2.java
@@ -0,0 +1,11 @@
+package com.selenium.programs;
+
+public class Test_Case_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is second test case");
+
+	}
+
+}
